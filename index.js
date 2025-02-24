393123d1d5733ebdeb26edf394d897cf9711e9d9
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-02-24 11:19:23 +0200

    secong commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+import express from "express"
+import mongoose, { mongo } from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+        .connect(MONGO_URL)
+        .then(()=>{
+            console.log("Database connected successfully.")
+            app.listen(PORT, ()=>{
+                console.log('Server is running on port :${PORT}')
+            })
+        })
+        .catch((error)=> console.log(error));
